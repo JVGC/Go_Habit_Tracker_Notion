8890bb610934caa45b864e9f73fb714939bb061c
JVGC <joaovictorgarciacoelho@gmail.com>
2023-03-25 11:43:03 -0300

    Returning habits percentage as strings with 2 points of precision

--- src/main.go
+++ src/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	notionapi "go_notion_api/src/notion_api"
+	"go_notion_api/src/notion_api/models"
 	usecases "go_notion_api/src/use_cases"
 
 	"github.com/gorilla/mux"
@@ -33,14 +35,20 @@ func HabitsRoute(w http.ResponseWriter, r *http.Request){
 }
 
 func PagesRoute(w http.ResponseWriter, r *http.Request){
-	res := notionapi.GetPages("")
+	res := notionapi.GetPages(models.Filter{Created_time: models.DateFilter{On_or_after: ""}})
 	json.NewEncoder(w).Encode(res)
 }
 
 func SumRoute(w http.ResponseWriter, r *http.Request){
-	date := r.URL.Query().Get("date")
-	res := usecases.HabitsPercentage(date)
-	json.NewEncoder(w).Encode(res)
+	startAt := r.URL.Query().Get("startAt")
+	res := usecases.HabitsPercentage(startAt)
+
+	returnObj := make(map[string]string)
+
+	for habit, value := range res{
+		returnObj[habit] = fmt.Sprintf("%.2f%%",value*100)
+	}
+	json.NewEncoder(w).Encode(returnObj)
 }
 
 
